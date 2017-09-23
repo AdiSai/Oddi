7a42920afe2d0978b64d348565126704c1ae719b
Dhruv Garg <dhruvgargj5@gmail.com>
2017-09-23 12:24:48 -0400

    timeline updated

--- app/src/main/java/com/adithyasairam/oddi/pojos/Class.java
+++ app/src/main/java/com/adithyasairam/oddi/pojos/Class.java
@@ -4,6 +4,51 @@ package com.adithyasairam.oddi.pojos;
  * Created by AdiSai on 9/23/17.
  */
 
-public class Class {
+public class Class
+{
+    private String name;
+    private String starT;
+    private String endT;
 
+    public Class (String n, String s, String e)
+    {
+        name = n;
+        starT = s;
+        endT = e;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String x)
+    {
+        this.x = name;
+    }
+
+    public String getStarT()
+    {
+        return starT;
+    }
+
+    public void setstarT (String t)
+    {
+        this.t = starT;
+    }
+
+    public String getendT()
+    {
+        return endT;
+    }
+
+    public void setendT(String e)
+    {
+        this.e = endT;
+    }
+
+    public String toString()
+    {
+        return name + " " +  starT + " " +  endT;
+    }
 }
