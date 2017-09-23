f5e01b312ce018f37df63aaa0fd76acb1b500e1d
Dhruv Garg <dhruvgargj5@gmail.com>
2017-09-23 09:49:13 -0400

    button intent

--- app/src/main/java/com/adithyasairam/oddi/ClassCreatorActivity.java
+++ app/src/main/java/com/adithyasairam/oddi/ClassCreatorActivity.java
@@ -1,11 +1,18 @@
 package com.adithyasairam.oddi;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class ClassCreatorActivity extends AppCompatActivity {
 
@@ -23,6 +30,31 @@ public class ClassCreatorActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void showTimePickerDialog(View v)
+    {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
     }
 
 }
