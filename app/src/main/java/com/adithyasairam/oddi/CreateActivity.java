624353a0b0be424ecd36579ebefd0baa278d588d
Dhruv Garg <dhruvgargj5@gmail.com>
2017-09-23 01:50:30 -0400

    activity 138am

--- app/src/main/java/com/adithyasairam/oddi/CreateActivity.java
+++ app/src/main/java/com/adithyasairam/oddi/CreateActivity.java
@@ -5,9 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private EditText mDateEntryField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,48 @@ public class CreateActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mDateEntryField = (EditText) findViewById(R.id.dateSelection);
     }
+    private TextWatcher mDateEntryWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String working = s.toString();
+            boolean isValid = true;
+            if (working.length()==2 && before ==0) {
+                if (Integer.parseInt(working) < 1 || Integer.parseInt(working)>12) {
+                    isValid = false;
+                } else {
+                    working+="/";
+                    mDateEntryField.setText(working);
+                    mDateEntryField.setSelection(working.length());
+                }
+            }
+            else if (working.length()==7 && before ==0) {
+                String enteredYear = working.substring(3);
+                int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+                if (Integer.parseInt(enteredYear) < currentYear) {
+                    isValid = false;
+                }
+            } else if (working.length()!=7) {
+                isValid = false;
+            }
+
+            if (!isValid) {
+                mDateEntryField.setError("Enter a valid date: MM/YYYY");
+            } else {
+                mDateEntryField.setError(null);
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    };
 
 }
