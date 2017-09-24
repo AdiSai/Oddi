3147f81117e88495bba4dd6d6f4b59d9b794d6cb
Adi Sai <adithya.sairam1@gmail.com>
2017-09-24 13:06:35 -0400

    Update stuff

--- app/src/main/java/com/adithyasairam/oddi/Adapters/TimeLineAdapter.java
+++ app/src/main/java/com/adithyasairam/oddi/Adapters/TimeLineAdapter.java
@@ -12,24 +12,11 @@ import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;package muralimohan.spreadsheetbuddy.Adapters;
-
-import android.content.Context;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import com.adithyasairam.oddi.R;
+import com.adithyasairam.oddi.pojos.Assignment;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import muralimohan.spreadsheetbuddy.R;
 
 /**
  * Created by Murali on 7/8/2017.
@@ -37,22 +24,20 @@ import muralimohan.spreadsheetbuddy.R;
 
 public class TimeLineAdapter extends BaseAdapter {
     private Context mContext;
-    private final List<String> varsList;
-    private final List<String> dataList;
+    private List<Assignment> assignmentList;
 
-    public TimeLineAdapter(Context c, List<String> varsList, List<String> dataList) {
+    public TimeLineAdapter(Context c, List<Assignment> assignments) {
         mContext = c;
-        this.varsList = varsList;
-        this.dataList = dataList;
+        assignmentList = assignments;
     }
 
     public int getCount() {
-        return varsList.size();
+        return assignmentList.size();
     }
 
     @Override
-    public String getItem(int p) {
-        return varsList.get(p);
+    public Assignment getItem(int p) {
+        return assignmentList.get(p);
     }
 
     @Override
@@ -69,20 +54,13 @@ public class TimeLineAdapter extends BaseAdapter {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            //list = new View(mContext);
-            convertView = inflater.inflate(R.layout.data_display_layout, null);
+            convertView = inflater.inflate(R.layout.activity_timeline, null);
             viewHolder = new ViewHolder();
-            viewHolder.editText = (EditText) convertView.findViewById(R.id.valueText);
-            viewHolder.editText.setTag(index);
-            viewHolder.textView = (TextView) convertView.findViewById(R.id.variableText);
-            viewHolder.id = index;
-            convertView.setTag(viewHolder);
         }
         else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        viewHolder.textView.setText(varsList.get(index));
-        viewHolder.editText.setText(dataList.get(index));
+        viewHolder.textView.setText(assignmentList.get(index));
 
         //checks for updates in the text in order to keep them changed in the ListView
         viewHolder.editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -101,25 +79,6 @@ public class TimeLineAdapter extends BaseAdapter {
 
     public String getDataPoint(int p){
         return dataList.get(p);
-    }
-
-    public int getIndexOfDataListMember(String member) throws IllegalArgumentException {
-        String lowercaseMember = member.toLowerCase();
-        List<String> matchingData = new ArrayList<String>();
-        for(String dataPoint : dataList){
-            if (dataPoint.toLowerCase().startsWith(lowercaseMember)) matchingData.add(dataPoint);
-        }
-
-        int possibleMatches = matchingData.size();
-        if (possibleMatches == 0) throw new IllegalArgumentException("No matches");
-        if (possibleMatches == 1){
-            String match = matchingData.get(0);
-            int index = dataList.indexOf(match);
-            return index;
-        }
-        else throw new IllegalArgumentException("Too many matches");
-
-
     }
 
     class ViewHolder{
